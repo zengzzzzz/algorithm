f6d77bab808437605237cc80eef861594088b8d7
zengzh <mrzengzzzz@gmail.com>
2022-12-26 15:27:37 +0000

    newskiplist test

---
+++ newskiplist/go.mod
@@ -0,0 +1,3 @@
+module newskiplist
+
+go 1.18
---
+++ newskiplist/skiplist1.go
@@ -0,0 +1,47 @@
+package skiplist1
+
+import (
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+const (
+	defaultMaxlevel    int     = 10
+	defaultProbability float64 = 1 / math.E
+)
+
+type elementNode struct {
+	next []*Element
+}
+
+type Element struct {
+	elementNode
+	key   float64
+	value interface{}
+}
+
+func (e *Element) Key() float64 {
+	return e.key
+}
+
+func (e *Element) Value() interface{} {
+	return e.value
+}
+
+type SkipList struct {
+	elementNone
+	maxLevel    int
+	length      int
+	randSource  rand.Source
+	probability float64
+	probTabale  []float64
+    mutex sync.RWMutex
+    prevNodeCache []*elementNode
+}
+
+func NewSkipList() *SkipList{
+    return NewWithMaxLevel(defaultMaxlevel)
+}
+
