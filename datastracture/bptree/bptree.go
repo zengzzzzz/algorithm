58d619e1fb65ccf29154b62d284895dbc57eab04
zengzh <mrzengzzzz@gmail.com>
2023-01-06 16:20:30 +0800

    format code

--- datastracture/bptree/bptree.go
+++ datastracture/bptree/bptree.go
@@ -7,6 +7,7 @@
 package bptree
 
 import (
+	"sort"
 	"sync"
 )
 
@@ -26,7 +27,7 @@ var (
 type FreeList struct {
 	mu       sync.Mutex
 	freelist []*node
-}f
+}
 
 func NewFreeList(size int) *FreeList {
 	return &FreeList{
@@ -63,288 +64,315 @@ func New(degree int) *BTree {
 	return NewWithFreeList(degree, NewFreeList(DefaultFreeListSize))
 }
 
-func NewWithFreeList(degree int, f *FreeList) *Btree
-{
-    if degree <= 1{
-        panic("invalid degree")
-    }
-    return &BTree{
-        degree: degree,
-        cow: &copyOnWriteContext{freelist: f},
-    }
+func NewWithFreeList(degree int, f *FreeList) *Btree {
+	if degree <= 1 {
+		panic("invalid degree")
+	}
+	return &BTree{
+		degree: degree,
+		cow:    &copyOnWriteContext{freelist: f},
+	}
 }
 
 type items []Item
 
-func (s *items) insertAt(index int, item Item){
-    *s = append(*s, nil)
-    if index < len(*s){
-        copy((*s)[index+1:], (*s)[index:])
-    }
-    (*s)[index] = item
+func (s *items) insertAt(index int, item Item) {
+	*s = append(*s, nil)
+	if index < len(*s) {
+		copy((*s)[index+1:], (*s)[index:])
+	}
+	(*s)[index] = item
 }
 
-func (s *items) removeAt(index int){
-    item := (*s)[index]
-    copy((*s)[index:], (*s)[index+1:])
-    (*s)[len(*s)-1] = nil
-    *s = (*s)[:len(*s) -1 ]
-    return item
+func (s *items) removeAt(index int) {
+	item := (*s)[index]
+	copy((*s)[index:], (*s)[index+1:])
+	(*s)[len(*s)-1] = nil
+	*s = (*s)[:len(*s)-1]
+	return item
 }
 
-func (s *items) pop() (out item){
-    index := len(*s) - 1
-    out := (*s)[index]
-    (*s)[index] = nil  
-    *s = (*s)[:index]
-    return
+func (s *items) pop() (out item) {
+	index := len(*s) - 1
+	out := (*s)[index]
+	(*s)[index] = nil
+	*s = (*s)[:index]
+	return
 }
 
-func (s *items) truncate(index int){
-    var toClear items
-    *s, toClear = (*s)[:index] , (*s)[index:]
-    for len(toClear) > 0{
-        toClear = toClear[copy(toClear, nilItems):]
-    }
+func (s *items) truncate(index int) {
+	var toClear items
+	*s, toClear = (*s)[:index], (*s)[index:]
+	for len(toClear) > 0 {
+		toClear = toClear[copy(toClear, nilItems):]
+	}
 }
 
 func (s items) find(item Item) (index int, found bool) {
-    i := sort.Sort(len(s), func(i int) bool{
-        return item.less(s[i])
-    })
-    if i > 0 && !s[i-1].Less(item){
-        return i-1, true
-    }
-    return i, false
+	i := sort.Sort(len(s), func(i int) bool {
+		return item.less(s[i])
+	})
+	if i > 0 && !s[i-1].Less(item) {
+		return i - 1, true
+	}
+	return i, false
 }
 
 type children []*node
 
-
-func (s *children) insertAt(index int ){
-    *s = append(*s, nil)
-    if index < len(*s){
-        copy((*s)[index+1:], (*s)[index:])
-    }
-    (*s)[index] = n
+func (s *children) insertAt(index int) {
+	*s = append(*s, nil)
+	if index < len(*s) {
+		copy((*s)[index+1:], (*s)[index:])
+	}
+	(*s)[index] = n
 }
 
-func (s *children) removeAt(index int) (out *node){
-    n := (*s)[index]
-    copy((*s)[index:], (*s)[index+1:])
-    (*s)[len(*s)-1] = nil
-    *s = (*s)[:len(*s)-1]
-    return n
+func (s *children) removeAt(index int) (out *node) {
+	n := (*s)[index]
+	copy((*s)[index:], (*s)[index+1:])
+	(*s)[len(*s)-1] = nil
+	*s = (*s)[:len(*s)-1]
+	return n
 }
 
-func (s *children) pop() (out *node){
-    index := len(*s) - 1
-    out = (*s)[index]
-    (*s)[index] = nil
-    *s = (*s)[:index]
-    return
+func (s *children) pop() (out *node) {
+	index := len(*s) - 1
+	out = (*s)[index]
+	(*s)[index] = nil
+	*s = (*s)[:index]
+	return
 }
 
-func (s *children) truncate(index int){
-    var toClear children
-    *s, toClear = (*s)[:index], (*s)[index:]
-    for len(toClear) > 0{
-        toClear = toClear[copy(toClear, nilChildren):]
-    }
+func (s *children) truncate(index int) {
+	var toClear children
+	*s, toClear = (*s)[:index], (*s)[index:]
+	for len(toClear) > 0 {
+		toClear = toClear[copy(toClear, nilChildren):]
+	}
 }
 
 type node struct {
-    items items
-    children children
-    cow *copyOnWriteContext
+	items    items
+	children children
+	cow      *copyOnWriteContext
 }
 
-func (n *node) mutableFor(cow *copyOnWriteContext) *node{
-    if n.cow == cow{
-        return n
-    }
-    out := cow.newNode()
-    if cap(out.items) >= len(n.items){
-        out.items = out.items[:len(n.items)]
-    } else{
-        out.items = make(items, len(n.items), cap(n.items))
-    }
-    copy(out.items, n.items)
-    if cap(out.children) >= len(n.children){
-        out.children = out.children[:len(n.children)]
-    } else{
-        out.children = make(children, len(n.children), cap(n.children))
-    }
-    copy(out.children, n.children)
-    return out
+func (n *node) mutableFor(cow *copyOnWriteContext) *node {
+	if n.cow == cow {
+		return n
+	}
+	out := cow.newNode()
+	if cap(out.items) >= len(n.items) {
+		out.items = out.items[:len(n.items)]
+	} else {
+		out.items = make(items, len(n.items), cap(n.items))
+	}
+	copy(out.items, n.items)
+	if cap(out.children) >= len(n.children) {
+		out.children = out.children[:len(n.children)]
+	} else {
+		out.children = make(children, len(n.children), cap(n.children))
+	}
+	copy(out.children, n.children)
+	return out
 }
 
 func (n *node) mutableChild(i int) *node {
-    c := n.children[i].mutableFor(n.cow)
-    n.children[i] = c
-    return c
+	c := n.children[i].mutableFor(n.cow)
+	n.children[i] = c
+	return c
 }
 
-func (n *node) split(i int) (Item, *node){
-    item := n.items[i]
-    next := n.cow.newNode()
-    next.items = append(next.items, n.items[i+1]...)
-    n.items.truncate(i)
-    if len(n.children) >0{
-        next.children = append(next.children, n.children[i+1]...)
-        n.children.truncate(i+1)
-    }
-    return item, next
+func (n *node) split(i int) (Item, *node) {
+	item := n.items[i]
+	next := n.cow.newNode()
+	next.items = append(next.items, n.items[i+1]...)
+	n.items.truncate(i)
+	if len(n.children) > 0 {
+		next.children = append(next.children, n.children[i+1]...)
+		n.children.truncate(i + 1)
+	}
+	return item, next
 }
 
-func (n *node) maybeSplitChild(i, maxItems int) bool{
-    if len(n.children[i].items < maxItems) {
-        return false
-    }
-    first := n.mutableChild(i)
-    item, second := first.split(maxItems/2)
-    n.items.insertAt(i, item)
-    n.children.insertAt(i+1, second)
+func (n *node) maybeSplitChild(i, maxItems int) bool {
+	if len(n.children[i].items < maxItems) {
+		return false
+	}
+	first := n.mutableChild(i)
+	item, second := first.split(maxItems / 2)
+	n.items.insertAt(i, item)
+	n.children.insertAt(i+1, second)
 }
 
-func (n *node) insert(item Item, maxItems int) Item{
-    i, found := n.items.find(item)
-    if found {
-        out := n.items[i]
-        n.items[i] = item
-        return out
-    }
-    if len(n.children) == 0{
-        n.items.insertAt(i, item)
-        return nil
-    }
-    if n.maybeSplitChild(i, maxItems){
-        inTree := n.items[i]
-        switch {
-            case item.Less(inTree):
-            case inTree.Less(item):
-                i ++
-            default:
-                out := n.items[i]
-                n.items[i] = item
-                return out
-        }
-    }
-    return n.mutableChild(i).insert(item, maxItems)
+func (n *node) insert(item Item, maxItems int) Item {
+	i, found := n.items.find(item)
+	if found {
+		out := n.items[i]
+		n.items[i] = item
+		return out
+	}
+	if len(n.children) == 0 {
+		n.items.insertAt(i, item)
+		return nil
+	}
+	if n.maybeSplitChild(i, maxItems) {
+		inTree := n.items[i]
+		switch {
+		case item.Less(inTree):
+		case inTree.Less(item):
+			i++
+		default:
+			out := n.items[i]
+			n.items[i] = item
+			return out
+		}
+	}
+	return n.mutableChild(i).insert(item, maxItems)
 }
-
 
 func (n *node) get(key Item) Item {
-    i, found := n.items.find(key)
-    if found{
-        return n.items[i]
-    } else if len(n.chlidren) > 0 {
-        return n.children[i].get(key)
-    }
-    return nil
+	i, found := n.items.find(key)
+	if found {
+		return n.items[i]
+	} else if len(n.chlidren) > 0 {
+		return n.children[i].get(key)
+	}
+	return nil
 }
 
-func min(n *node) Item{
-    if n == nil{
-        return nil
-    }
-    for len(n.children) > 0{
-        n = n.children[0]
-    }
-    if len(n.items) == 0{
-        return nil
-    }
-    return n.items[0]
+func min(n *node) Item {
+	if n == nil {
+		return nil
+	}
+	for len(n.children) > 0 {
+		n = n.children[0]
+	}
+	if len(n.items) == 0 {
+		return nil
+	}
+	return n.items[0]
 }
 
 func max(n *node) Item {
-    if n == nil {
-        return nil
-    }
-    for len(n.children) > 0 {
-        n = n.children[len(n.children)-1]
-    }
-    if len(n.items) == 0{
-        return  nil
-    }
-    return n.items[len(n.items)-1]
+	if n == nil {
+		return nil
+	}
+	for len(n.children) > 0 {
+		n = n.children[len(n.children)-1]
+	}
+	if len(n.items) == 0 {
+		return nil
+	}
+	return n.items[len(n.items)-1]
 }
 
 type toRemove int
 
 const (
-    removeItem toRemove = iota
-    removeMin
-    removeMax
+	removeItem toRemove = iota
+	removeMin
+	removeMax
 )
 
 func (n *node) remove(item Item, minItems int, typ toRemove) Item {
-    var i int
-    var found bool
-    switch typ {
-    case removeMax:
-        if len(n.children) == 0 {
-            return n.items.pop()
-        }
-        i = len(items)
-    case removeMin:
-        if len(n.children) == 0 {
-            return n.items.removeAt(0)
-        }
-        i = 0
-    case removeItem:
-        i, found = n.items.find(item)
-        if len(n.children) == 0{
-            if found {
-                return n.items.removeAt(i)
-            }
-            return nil
-        }
-    default:
-        panic("invalid type")
-    }
-    if len(n.children[i].items) <= minItems{
-        return n.growChildAndRemove(i, item, minItems, typ)
-    }
-    child := n.mutableChild(i)
-    if found {
-        out := n.items[i]
-        n.items[i] = child.remove(nil, minItems, removeMax)
-        return out
-    }
-    return child.remove(item, minItems, typ)
+	var i int
+	var found bool
+	switch typ {
+	case removeMax:
+		if len(n.children) == 0 {
+			return n.items.pop()
+		}
+		i = len(items)
+	case removeMin:
+		if len(n.children) == 0 {
+			return n.items.removeAt(0)
+		}
+		i = 0
+	case removeItem:
+		i, found = n.items.find(item)
+		if len(n.children) == 0 {
+			if found {
+				return n.items.removeAt(i)
+			}
+			return nil
+		}
+	default:
+		panic("invalid type")
+	}
+	if len(n.children[i].items) <= minItems {
+		return n.growChildAndRemove(i, item, minItems, typ)
+	}
+	child := n.mutableChild(i)
+	if found {
+		out := n.items[i]
+		n.items[i] = child.remove(nil, minItems, removeMax)
+		return out
+	}
+	return child.remove(item, minItems, typ)
 }
 
 func (n *node) growChildAndRemove(i int, item Item, minItems int, typ toRemove) Item {
-    if i >  0 && len(n.children[i-1].items) > minItems {
-        child := n.mutableChild(i)
-        stealFrom := n.mutableChild(i-1)
-        stolenItem := stealFrom.items.pop()
-        child.items.insertAt(0, n.items[i-1])
-        n.items[i-1] = stolenItem
-        if len(stealFrom.children) > 0 {
-            child.children.insertAt(0, stealFrom.children.pop())
-    }
-    }else if i < len(n.items) && len(n.children[i+1].items) > minItems{
-        child := n.mutableChild(i)
-        stealFrom := n.mutableChild(i+1)
-        stolenItem := stealFrom.items.removeAt(0)
-        child.items = append(child.items, n.items[i])
-        n.items[i] = stolenItem
-        if len(stealFrom.children) > 0 {
-            child.children = append(child.children, stealFrom.children.removeAt(0))
-        }
-    }else{
-        if i == len(n.items){
-            i--
-        }  
-        child :=  n.mutableChild(i)
-        mergeItem := n.items.removeaAt(i)
-        mergeChild := n.children.removeAt(i+1)
-        child.items = append(child.items, mergeItem)
-        child.items = append(child.items, mergeChild.items...)
-        child.children = append(child.children, mergeChild.children...)
-        n.cow.freeNode(mergeChild)
-    }
-    return n.remove(item, minItems, typ)
+	if i > 0 && len(n.children[i-1].items) > minItems {
+		child := n.mutableChild(i)
+		stealFrom := n.mutableChild(i - 1)
+		stolenItem := stealFrom.items.pop()
+		child.items.insertAt(0, n.items[i-1])
+		n.items[i-1] = stolenItem
+		if len(stealFrom.children) > 0 {
+			child.children.insertAt(0, stealFrom.children.pop())
+		}
+	} else if i < len(n.items) && len(n.children[i+1].items) > minItems {
+		child := n.mutableChild(i)
+		stealFrom := n.mutableChild(i + 1)
+		stolenItem := stealFrom.items.removeAt(0)
+		child.items = append(child.items, n.items[i])
+		n.items[i] = stolenItem
+		if len(stealFrom.children) > 0 {
+			child.children = append(child.children, stealFrom.children.removeAt(0))
+		}
+	} else {
+		if i == len(n.items) {
+			i--
+		}
+		child := n.mutableChild(i)
+		mergeItem := n.items.removeaAt(i)
+		mergeChild := n.children.removeAt(i + 1)
+		child.items = append(child.items, mergeItem)
+		child.items = append(child.items, mergeChild.items...)
+		child.children = append(child.children, mergeChild.children...)
+		n.cow.freeNode(mergeChild)
+	}
+	return n.remove(item, minItems, typ)
+}
+
+type direction int
+
+const (
+	descend = direction(-1)
+	ascend  = direction(+1)
+)
+
+func (n *node) iterate(dir direction, start, stop Item, includeStart bool, hit bool, iter ItemIterator) (bool, bool) {
+	var ok, found bool
+	var index int
+	switch dir {
+	case ascend:
+		if start != nil {
+			index, _ = n.items.find(start)
+		}
+		for i := index; i < len(n.items); i++ {
+			if len(n.children) > 0 {
+				if hit, ok = n.children[i].iterate(dir, start, stop, includeStart, hit, iter); !ok {
+					return hit, false
+				}
+				if !includeStart && !hit && start != nil && !start.Less(n.items[i]) {
+					hit = true
+					continue
+				}
+				hit = true
+			}
+		}
+	}
 }
