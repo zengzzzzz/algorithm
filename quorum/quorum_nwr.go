7d6c1e9b3b2095787ef26164ca585ccc403b1a27
zengzh <mrzengzzzz@gmail.com>
2023-05-09 09:14:45 +0000

    add quorum nwr test

--- quorum/quorum_nwr.go
+++ quorum/quorum_nwr.go
@@ -26,9 +26,9 @@ func (n *Node) Write(data *Data) {
 	fmt.Printf("Node %d write data %s\n", n.ID, data.Value)
 }
 
-func (n *Node) Read(seq int) *Data {
+func (n *Node) Read(Value string) *Data {
 	for _, data := range n.DataList {
-		if data.Seq == seq {
+		if data.Value == Value {
 			fmt.Printf("Node %d read data %s\n", n.ID, data.Value)
 			return data
 		}
@@ -36,48 +36,9 @@ func (n *Node) Read(seq int) *Data {
 	return nil
 }
 
-package main
-
-import (
-	"fmt"
-	"sync"
-)
-
-// 定义一个结构体来存储数据
-type Data struct {
-	Value string // 数据的值
-	Seq   int    // 数据的序列号
-}
-
-// 定义一个结构体来表示节点
-type Node struct {
-	ID       int      // 节点的 ID
-	DataList []*Data // 节点存储的数据列表
-}
-
-// 实现一个写操作
-func (n *Node) Write(data *Data) {
-	n.DataList = append(n.DataList, data)
-	fmt.Printf("Node %d writes data: %v\n", n.ID, data)
-}
-
-// 实现一个读操作
-func (n *Node) Read(seq int) *Data {
-	for _, data := range n.DataList {
-		if data.Seq == seq {
-			fmt.Printf("Node %d reads data: %v\n", n.ID, data)
-			return data
-		}
-	}
-	return nil
-}
-
-// 实现 Quorum NW 算法的写操作
 func quorumNWWrite(nodes []*Node, data *Data, quorum int) bool {
 	var wg sync.WaitGroup
 	count := 0
-
-	// 遍历写组中的节点进行写操作
 	for _, node := range nodes[:quorum] {
 		wg.Add(1)
 		go func(node *Node) {
@@ -87,78 +48,44 @@ func quorumNWWrite(nodes []*Node, data *Data, quorum int) bool {
 		}(node)
 	}
 	wg.Wait()
-
-	// 如果写操作无法达到写组的阈值，则返回错误
-	if count < quorum {
-		return false
-	}
-
-	// 如果写操作成功，则返回成功
-	return true
+	return count >= quorum
 }
 
-// 实现 Quorum NR 算法的读操作
-func quorumNRRead(nodes []*Node, seq int, quorum int) *Data {
+func quorumNWRead(nodes []*Node, value string, quorum int) *Data {
 	var wg sync.WaitGroup
-	dataList := make(chan *Data, len(nodes))
-
-	// 遍历读组中的节点进行读操作
+	datalist := make(chan *Data, len(nodes))
 	for _, node := range nodes[:quorum] {
 		wg.Add(1)
 		go func(node *Node) {
 			defer wg.Done()
-			data := node.Read(seq)
+			data := node.Read(value)
 			if data != nil {
-				dataList <- data
+				datalist <- data
 			}
 		}(node)
 	}
 	wg.Wait()
-
-	// 如果读操作无法达到读组的阈值，则返回 nil
-	if len(dataList) < quorum {
+	close(datalist)
+	if len(datalist) < quorum {
 		return nil
 	}
-
-	// 如果读操作成功，则返回最新的数据
-	var latestData *Data
-	for data := range dataList {
-		if latestData == nil || data.Seq > latestData.Seq {
-			latestData = data
+	var lastestData *Data
+	for data := range datalist {
+		if lastestData == nil || data.Seq > lastestData.Seq {
+			lastestData = data
 		}
 	}
-	return latestData
+	return lastestData
 }
 
-// 实现 Quorum NWR 算法
-func quorumNWR(nodes []*Node, data *Data, writeQuorum, readQuorum int) bool {
-	// 进行写操作
-	ok := quorumNWWrite(nodes, data, writeQuorum)
+func quorumNWR(node []*Node, data *Data, writeQuorum, readQuorum int) bool {
+	ok := quorumNWWrite(node, data, writeQuorum)
 	if !ok {
 		return false
 	}
-
-	// 进行读操作
-	readData := quorumNRRead(nodes, data.Seq, readQuorum)
+	readData := quorumNWRead(node, data.Value, readQuorum)
 	if readData == nil || readData.Value != data.Value {
 		return false
 	}
-
-	// 如果写和读操作均成功，则返回成功
 	return true
-}
-
-func main() {
-	// 创建三个节点
-	node1 := &Node{ID: 1}
-	node2 := &Node{ID: 2}
-	node3 := &Node{ID: 3}
-	nodes := []*Node{node1, node2, node3}
-
-	// 定义一个数据
-	data := &Data{Value: "Hello, world!", Seq: 1}
-
-	// 调用 Quorum NWR 算法进行写操作和读操作
-	ok := quorumNWR(nodes, data, 2, 2)
-	fmt.Printf("Quorum NWR result: %v\n", ok)
 }
--- quorum/quorum_nwr_test.go
+++ quorum/quorum_nwr_test.go
@@ -1,6 +1,91 @@
 /*
- * @Author: zengzh 
- * @Date: 2023-05-09 12:31:33 
- * @Last Modified by:   zengzh 
- * @Last Modified time: 2023-05-09 12:31:33 
+ * @Author: zengzh
+ * @Date: 2023-05-09 12:31:33
+ * @Last Modified by:   zengzh
+ * @Last Modified time: 2023-05-09 12:31:33
  */
+package quorum
+
+import (
+	"testing"
+)
+
+func TestNodeWrite(t *testing.T) {
+	node := &Node{1, nil}
+	data := &Data{"data1", 1}
+	node.Write(data)
+	if len(node.DataList) != 1 || node.DataList[0].Value != "data1" {
+		t.Errorf("Node write data failed")
+	}
+}
+
+func TestNodeRead(t *testing.T) {
+	node := &Node{1, []*Data{{Value: "data1", Seq: 1}}}
+	data := node.Read("data1")
+	if data.Value != "data1" {
+		t.Errorf("Node read data failed")
+	}
+	data = node.Read("deta2")
+	if data != nil {
+		t.Errorf("Node read data should fail")
+	}
+}
+
+func TestQuorumNWWrite(t *testing.T) {
+	nodes := []*Node{
+		{1, nil},
+		{2, nil},
+		{3, nil},
+	}
+	data := &Data{"data1", 1}
+	// quorum is 2, should succeed
+	ok := quorumNWWrite(nodes, data, 2)
+	if !ok {
+		t.Errorf("Expected quorumNWWrite to succeed")
+	}
+
+	// quorum is 3, should fail
+	ok = quorumNWWrite(nodes, data, 3)
+	if !ok {
+		t.Errorf("Expected quorumNWWrite to fail")
+	}
+}
+func TestQuorumNWRead(t *testing.T) {
+	nodes := []*Node{
+		{1, []*Data{{Value: "data1", Seq: 1}}},
+		{2, []*Data{{Value: "data1", Seq: 2}}},
+		{3, []*Data{{Value: "data2", Seq: 2}}},
+	}
+	// quorum is 2, should read data with seq 1
+	data := quorumNWRead(nodes, "data1", 2)
+	if data.Value != "data1" {
+		t.Errorf("Expected to read data1, actual is %s", data.Value)
+	}
+
+	// quorum is 3, should fail to read
+	data = quorumNWRead(nodes, "data2", 3)
+	if data != nil {
+		t.Errorf("Expected read to fail")
+	}
+}
+
+func TestQuorumNWR(t *testing.T) {
+	nodes := []*Node{
+		{1, nil},
+		{2, nil},
+		{3, nil},
+	}
+	data := &Data{"data1", 1}
+
+	// Write quorum is 2, read quorum is 2, should succeed
+	ok := quorumNWR(nodes, data, 2, 2)
+	if !ok {
+		t.Errorf("Expected quorumNWR to succeed")
+	}
+
+	// Write quorum is 2, read quorum is 3, should fail
+	ok = quorumNWR(nodes, data, 2, 3)
+	if ok {
+		t.Errorf("Expected quorumNWR to fail")
+	}
+}
